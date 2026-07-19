5ffc71278bee004abc40d9ee06cdc778b88fe3e7
moluopro <moluopro@qq.com>
2026-07-19 16:38:17 +0800

    test(matlab): add sparse index differential coverage

--- tests/differential/corpus.cmake
+++ tests/differential/corpus.cmake
@@ -46,6 +46,9 @@ mpf_add_differential_case(
   matlab-sparse-construction-transpose matlab
   examples/matlab/sparse_construction_transpose.m "0 0 2 2 2 2 2 1 1 4 5 5 5")
 mpf_add_differential_case(
+  matlab-sparse-indexing matlab examples/matlab/sparse_indexing.m
+  "3 4 5 3 1 2 1 4 3 2 5 1 4 3 2 5 5 5 1 4 2 5 5 1 5 5 1 0 0 2 1 5 2 1 2 1 1 2 2 1 1 1")
+mpf_add_differential_case(
   matlab-sparse-singular-square-warning matlab
   examples/matlab/sparse_singular_square_warning.m "0 4" tokens
   "matrix is singular to working precision")
